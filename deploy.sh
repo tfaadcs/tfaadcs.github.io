71cffb1b5f37467efb9eac6e96fe36574445f283
Jorge Mario Cruz-Duarte <jorge.cruz@itesm.mx>
2024-04-03 11:39:58 -0600

    automatic local deployment (test)

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 # create a local gh-pages branch containing the splitted output folder
-git subtree split --prefix _site -b gh-pages
-
-bundle exec jekyll build
+#git subtree split --prefix _site -b gh-pages
 
 # force the push of the gh-pages branch to the remote gh-pages branch at origin
-git push -f origin gh-pages:gh-pages
+#git push -f origin gh-pages:gh-pages
 
 # delete the local gh-pages because you will need it later
-git branch -D gh-pages
+#git branch -D gh-pages
+
+# Build the website locally
+bundle exec jekyll build
+
+# Copy the content from _site to the local repository tfaadcs.github.io
+cp _site/* ../.
